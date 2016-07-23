9b85972c3f6e9d9980ee5014a61557384c6e8b8f
Anoop <anoopkrsonkar@gmail.com>
2016-07-23 14:45:35 +0530

    hi

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 <%=javascript_include_tag 'jquery-1.11.1','jquery.validate', :defer=>'defer' %><%=javascript_include_tag 'validation','bootstrap.min','bootstrap-select.min','fileinput.min','html5','search','jquery-ui', :defer=>'defer' %>
 https://github.com/anoopkrsonkar/testing.git
+kjhbsdcijdhbsodkc
